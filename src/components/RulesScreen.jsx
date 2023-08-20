8d6639bb850c81943abdf085de8e0ebfc0d6f5ea
roodhouse <rughjm@gmail.com>
2023-08-20 14:49:20 -0500

    rule screen done

--- src/components/RulesScreen.jsx
+++ src/components/RulesScreen.jsx
@@ -7,17 +7,17 @@ import ConfirmButton from './rulesScreen/ConfirmButton'
 function RulesScreen() {
   return (
     <>
-        <div id="rulesScreenContainer" className='bg-white rounded-[40px] pt-[30px] px-5 pb-[63px] flex flex-col items-center border-[3px] border-black shadow-[0px_10px_0px_0px_#000]'>
-            <div id="rulesHeaderWrapper" className='mb-[29px]'>
+        <div id="rulesScreenContainer" className='bg-white rounded-[40px] pt-[30px] px-5 pb-[63px] flex flex-col items-center border-[3px] border-black shadow-[0px_10px_0px_0px_#000] md:items-start'>
+            <div id="rulesHeaderWrapper" className='mb-[29px] md:self-center'>
                 <RulesHeader />
             </div>
             <div id="objectiveWrapper" className='mb-[33px]'>
                 <Objective />
             </div>
-            <div id="howToPlayWrapper">
+            <div id="howToPlayWrapper" className='self-start'>
                 <HowToPlay />
             </div>
-            <div id="confirmButtonWrapper" className='absolute bottom-[15%]'>
+            <div id="confirmButtonWrapper" className='relative w-16 md:self-center'>
                 <ConfirmButton />
             </div>
         </div>
