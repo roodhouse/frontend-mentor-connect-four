199a3ab9f7a5d73eef5eb650f7a1572c32a8e544
roodhouse <rughjm@gmail.com>
2023-08-20 11:44:42 -0500

    tab homescreen done

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -7,7 +7,7 @@ import GameRules from './GameRules'
 function HomeScreen() {
   return (
     <>
-        <div id="homeScreenContainer" className='flex flex-col items-center'>
+        <div id="homeScreenContainer" className='flex flex-col items-center md:w-[480px] md:h-[507px] md:pt-[70px] md:pb-[60px] md:justify-around md:bg-purple md:rounded-[40px] md:border-[3px] md:border-black md:shadow-[0px_10px_0px_0px_black]'>
             <div id="logoWrapper" className='mb-[79px]'>
                 <Logo />
             </div>
@@ -18,7 +18,7 @@ function HomeScreen() {
                 <TwoPlayer />
             </div>
             <div id="gameRulesWrapper">
-                {/* <GameRules /> */}
+                <GameRules />
             </div>
         </div>
     </>
