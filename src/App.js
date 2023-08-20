464e709ba802dce8133092f7b2b64a9b908fbcfc
roodhouse <rughjm@gmail.com>
2023-08-20 11:53:44 -0500

    home screen done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div id='mainWrapper'>
-        <div id='mainContainer' className='bg-purple h-screen flex justify-center items-center px-5'>
+        <div id='mainContainer' className='bg-purple h-screen flex justify-center items-center px-5 md:bg-darkPurple'>
           <div id='homeScreenWrapper'>
             <HomeScreen />
           </div>
