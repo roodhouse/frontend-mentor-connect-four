c6113713b8a5b0dce4b0296bbe3f15787b7a478e
roodhouse <rughjm@gmail.com>
2023-08-21 13:30:49 -0500

    updates

--- src/components/gameOn/playerCard/PlayerOne.jsx
+++ src/components/gameOn/playerCard/PlayerOne.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function PlayerOne() {
   return (
-    <div>PlayerOne</div>
+    <>
+        <div id="playerOneContainer">
+            <div id="playerOneBlock">
+                
+            </div>
+            <div id="playerOneFace">
+                <div className='w-[54px] h-[59px] bg-[url("./assets/images/player-one.svg")] bg-no-repeat' />
+            </div>
+        </div>
+    </>
   )
 }
 
