4070d2398ad16cf08c46ac5e38abca5ee18c053a
roodhouse <rughjm@gmail.com>
2023-08-21 08:24:52 -0500

    fixed home screen and color issue

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen';
 import RulesScreen from './components/RulesScreen';
 
 // need to go back and adjust the cpu game based on the bonus specs
 // bug 1: home screen needs to be dark purple at 768 and greater
+    // change the background color on click of the buttons
 
 function App() {
 
   return (
     <div className="App">
       <div id='mainWrapper'>
-        <div id='mainContainer' className='bg-purple h-screen flex justify-center items-center px-5'>
-          <div id='homeScreenWrapper' className='hidden'>
+        <div id='mainContainer' className='bg-purple md:bg-darkPurple h-screen flex justify-center items-center px-5'>
+          <div id='homeScreenWrapper' className=''>
             <HomeScreen />
           </div>
-          <div id='rulesScreenWrapper' className='w-full max-w-[480px]'>
+          <div id='rulesScreenWrapper' className='hidden w-full max-w-[480px]'>
             <RulesScreen />
           </div>
         </div>
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -5,6 +5,20 @@ import TwoPlayer from './homeScreen/TwoPlayer'
 import GameRules from './homeScreen/GameRules'
 
 function HomeScreen() {
+
+    function hideHome() {
+        // hide the home screen
+        document.getElementById('homeScreenWrapper').style.display = 'none'
+        // change the main screen background color
+        document.getElementById('mainContainer').style.backgroundColor = '#7945FF'
+    }
+
+    const handleRulesClick = (e) => {
+        // hide the home screen
+        hideHome()
+        // display rules module
+        document.getElementById('rulesScreenWrapper').classList.remove('hidden')
+    }
   return (
     <>
         <div id="homeScreenContainer" className='flex flex-col items-center md:w-[480px] md:h-[507px] md:pt-[70px] md:pb-[60px] md:justify-around md:bg-purple md:rounded-[40px] md:border-[3px] md:border-black md:shadow-[0px_10px_0px_0px_black]'>
@@ -17,7 +31,7 @@ function HomeScreen() {
             <div id="twoPlayerWrapper" className='mb-[30px]'>
                 <TwoPlayer />
             </div>
-            <div id="gameRulesWrapper">
+            <div id="gameRulesWrapper" onClick={handleRulesClick}>
                 <GameRules />
             </div>
         </div>
