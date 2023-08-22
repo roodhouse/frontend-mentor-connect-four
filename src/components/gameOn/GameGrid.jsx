30bf44ee3c7bc730a5484a335f5160a7256fafff
roodhouse <rughjm@gmail.com>
2023-08-22 11:05:54 -0500

    game grid on

--- src/components/gameOn/GameGrid.jsx
+++ src/components/gameOn/GameGrid.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function GameGrid() {
   return (
     <>
-        <div id="gameGridContainer">
-            <div id="whiteLayer" className='w-[335px] h-[310px] bg-[url("./assets/images/board-layer-white-small.svg")]'/>
+        <div id="gameGridContainer" className='relative h-[310px]'>
+            <div id="whiteLayer" className='w-[335px] h-[310px] bg-[url("./assets/images/board-layer-white-small.svg")] absolute z-20'/>
+            <div id="blackLayer" className='w-[335px] h-[310px] bg-[url("./assets/images/board-layer-black-small.svg")] absolute z-10 bottom-[-5px] rounded-[20px]'/>
         </div>
     </>
   )
