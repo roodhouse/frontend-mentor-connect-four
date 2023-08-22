227ad05a9239f744447964ba1e6fb29baa53ff9d
roodhouse <rughjm@gmail.com>
2023-08-22 14:38:15 -0500

    added notes

--- src/components/gameOn/gameGrid/WhiteCircleContainer.jsx
+++ src/components/gameOn/gameGrid/WhiteCircleContainer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function WhiteCircleContainer() {
   return (
-    <div>WhiteCircleContainer</div>
+    <>
+      <div id="whiteCircles">
+
+        {/* use below as template */}
+      {/* <div id={'parentSquare'+index} className='parentSquare w-[96px] h-[96px] bg-semiDarkNavy rounded-[10px] shadow-[inset_0px_-8px_0px_0px_#10212A] flex justify-center px-[28px] py-6 mb-5 md:w-[140px] md:h-[140px] md:items-center cursor-pointer'>
+            <Square turn={turn} changeTurn={changeTurn} index={index} playerXarray={playerXarray} playerOarray={playerOarray} restarted={restarted} newRound={newRound} theCompetition={theCompetition} theGrid={theGrid} updateGrid={updateGrid} cpuChoice={cpuChoice} />
+        </div> */}
+      </div>
+    </>
   )
 }
 
