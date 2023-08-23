8dcb918becf5bcc41bf537b070fd8f80e81f855a
roodhouse <rughjm@gmail.com>
2023-08-23 11:59:50 -0500

    building out grid

--- src/components/gameOn/GameGrid.jsx
+++ src/components/gameOn/GameGrid.jsx
@@ -9,31 +9,32 @@ import CircleContainer from './gameGrid/CircleContainer'
 function GameGrid() {
   return (
     <>
-        <div id="gameGridContainer" className='relative h-[310px'>
-          {/* <div id="whiteGrid" className='bg-white'>
+        <div id="gameGridContainer" className='relative h-[310px]'>
+          <div id="whiteGrid" className='bg-white h-full rounded-[20px] border-[3px] border-black grid grid-cols-7 w-[335px] px-[10px] pt-[10px] pb-8 justify-items-center items-center'>
           {Array.from({ length: 42 }).map((_, index) => (
                 <WhiteCircleContainer key={index}  index={index} />
             ))}
           </div>
 
-          <div id="blackGrid" className='bg-black'>
+          {/* <div id="blackGrid" className='bg-black'>
           {Array.from({ length: 42 }).map((_, index) => (
                 <BlackCircleContainer key={index}  index={index} />
             ))}
           </div> */}
         
-            <div id="whiteLayer" className=' hidden w-[335px] h-[310px] bg-[url("./assets/images/board-layer-white-small.svg")] absolute z-20'/>
-            {/* <div id="circleLayerWrapper" className='w-[335px] h-[310px] flex flex-wrap ml-[10px] items-center pt-[1px]'>
+            {/* <div id="whiteLayer" className=' hidden w-[335px] h-[310px] bg-[url("./assets/images/board-layer-white-small.svg")] absolute z-20'/>
+            <div id="circleLayerWrapper" className='w-[335px] h-[310px] flex flex-wrap ml-[10px] items-center pt-[1px]'>
               {Array.from({ length: 42}).map((_, index) => (
                 <CircleContainer key={index} index={index} />
               ))}
-            </div> */}
+            </div>
+
             <div id="circleLayerWrapper" className='w-[335px] h-[310px] grid grid-cols-7'>
               {Array.from({ length: 42}).map((_, index) => (
                 <CircleContainer key={index} index={index} />
               ))}
             </div>
-            <div id="blackLayer" className='w-[335px] h-[310px] bg-[url("./assets/images/board-layer-black-small.svg")] absolute z-10 rounded-[20px] shadow-[0px_10px_0px_0px_#000]'/>
+            <div id="blackLayer" className='w-[335px] h-[310px] bg-[url("./assets/images/board-layer-black-small.svg")] absolute z-10 rounded-[20px] shadow-[0px_10px_0px_0px_#000]'/> */}
         </div>
     </>
   )
--- src/components/gameOn/gameGrid/WhiteCircleContainer.jsx
+++ src/components/gameOn/gameGrid/WhiteCircleContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WhiteCircleContainer() {
   return (
     <>
-      <div id="whiteCircles">
+      <div id="whiteCircles" className='w-[34px] h-[34px] bg-purple rounded-[50%] border-[3px] border-black'>
 
         {/* use below as template */}
       {/* <div id={'parentSquare'+index} className='parentSquare w-[96px] h-[96px] bg-semiDarkNavy rounded-[10px] shadow-[inset_0px_-8px_0px_0px_#10212A] flex justify-center px-[28px] py-6 mb-5 md:w-[140px] md:h-[140px] md:items-center cursor-pointer'>
