2c2262dbace3868a2422192cf94eee16ab753cd8
roodhouse <rughjm@gmail.com>
2023-08-24 14:02:28 -0500

    rules and main full view done

--- src/components/homeScreen/TwoPlayer.jsx
+++ src/components/homeScreen/TwoPlayer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TwoPlayer() {
   return (
     <>
-      <div id="twoPlayerContainer" className='w-[335px] h-[72px] rounded-[20px] border-[3px] border-black shadow-[0px_10px_0px_0px_black] bg-yellow flex pl-5 pr-[17px] justify-between items-center cursor-pointer hover:border-darkPurple hover:shadow-[0px_10px_0px_0px_#5C2DD5]'>
+      <div id="twoPlayerContainer" className='w-[335px] md:w-[400px] h-[72px] rounded-[20px] border-[3px] border-black shadow-[0px_10px_0px_0px_black] bg-yellow flex pl-5 pr-[17px] justify-between items-center cursor-pointer hover:border-darkPurple hover:shadow-[0px_10px_0px_0px_#5C2DD5]'>
           <div id="twoPlayerCopy" className='text-2xl leading-normal font-bold text-black'>
             <p>PLAY VS PLAYER</p>
           </div>
