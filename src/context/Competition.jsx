f1cde577fe25d980c08bf7cffc6dfefaa9468d6d
roodhouse <rughjm@gmail.com>
2023-08-24 19:05:01 -0500

    competition context

---
+++ src/context/Competition.jsx
@@ -0,0 +1,27 @@
+import React, { useContext, createContext, useState } from 'react'
+
+// create the context
+const CompetitionContext = createContext()
+
+// define a provider component to wrap the app
+const CompetitionProvider = ({ children }) => {
+    // define the function you want to share
+    const [ competition, setCompetition ] = useState('')
+    return (
+        <CompetitionContext.Provider value={{ competition, setCompetition }}>
+            {children}
+        </CompetitionContext.Provider>
+    )
+}
+
+// create a custom hook for using the context
+const useCompetition = () => {
+    const context = useContext(CompetitionContext)
+    if (!context) {
+        throw new Error('useCompetition must be used within a CompetitionProvider')
+    }
+    return context
+}
+
+
+export { CompetitionProvider, useCompetition }
